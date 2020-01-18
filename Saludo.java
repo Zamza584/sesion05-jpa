f4e21146ed9d62442bfc0c35bcccdbff537ae830
Manuel <mzama@gmail.com>
2020-01-18 15:45:36 -0500

    Actualizado

---
+++ Saludo.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.springbootapp.beans;
+
+public interface Saludo {
+
+	public String saludarDeDia();
+	
+	public String saludarDeTarde();
+	
+	public String saludarDeNoche();
+	
+}
